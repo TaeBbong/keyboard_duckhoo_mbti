66ca22628987cc6eb3a8f23033c8b47a2a5de220
TaeBbong <mok05289@korea.ac.kr>
2021-08-15 21:16:49 +0900

    [ADD] Styled-Components

--- src/App.js
+++ src/App.js
@@ -1,25 +1,33 @@
-import logo from './logo.svg';
-import './App.css';
+import styled from "styled-components";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container>
+      <Button />
+      <Button danger />
+    </Container>
   );
 }
+
+const Container = styled.div`
+  width: 100%;
+  height: 100vh;
+  bakcground-color: #bdc3c7;
+`;
+
+const Button = styled.button`
+  border-radius: 50px;
+  padding: 5px;
+  min-width: 120px;
+  color: white;
+  font-weight: 600;
+  -webkit-appearance: none;
+  cursor: pointer;
+  background-color: ${(props) => (props.danger ? "#e74c37" : "2ecc71")};
+  &:active,
+  &:foucus {
+    outline: none;
+  }
+`;
 
 export default App;
